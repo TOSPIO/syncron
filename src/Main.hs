d0df365c5cb414cd84ec80b22ef298102431b32b
kenneth.zhao <kenneth.zhao@netis.com.cn>
2015-08-06 20:49:03 +0800

    Added move event handling. Refactored a little bit.

--- src/Main.hs
+++ src/Main.hs
@@ -16,18 +16,19 @@ The main module
 
 module Main where
 
-import System.Environment
-import System.FilePath ((</>), joinPath, addTrailingPathSeparator, normalise, isAbsolute)
-import System.Directory (doesDirectoryExist, getDirectoryContents, getHomeDirectory)
-import System.INotify
-import System.Process
-import System.IO.Unsafe
 import Control.Concurrent (threadDelay)
 import Control.Monad
-import System.Path.NameManip (guess_dotdot, absolute_path)
+import qualified Data.HashMap.Strict as HM
 import Data.Maybe (fromJust)
 import Data.List (isPrefixOf)
 import Data.IORef
+import System.Directory (doesDirectoryExist, getDirectoryContents, getHomeDirectory)
+import System.Environment
+import System.FilePath ((</>), joinPath, addTrailingPathSeparator, normalise, isAbsolute)
+import System.INotify
+import System.IO.Unsafe
+import System.Path.NameManip (guess_dotdot, absolute_path)
+import System.Process
 
 absolutize :: String -> IO String
 absolutize aPath
@@ -57,7 +58,7 @@ getSubDirs topDir = do
   return $ topDir:concat paths
 
 eventVarieties :: [EventVariety]
-eventVarieties = [Modify, Attrib, Create, Delete, DeleteSelf, MoveSelf]
+eventVarieties = [Modify, Attrib, Create, Delete, MoveIn, MoveOut]
 
 
 {-# NOINLINE baseSrcDirRef #-}
@@ -67,6 +68,91 @@ baseSrcDirRef = unsafePerformIO $ newIORef ""
 {-# NOINLINE baseDstDirRef #-}
 baseDstDirRef :: IORef FilePath
 baseDstDirRef = unsafePerformIO $ newIORef ""
+
+{-# NOINLINE dirWDMapperRef #-}
+dirWDMapperRef :: IORef (HM.HashMap FilePath WatchDescriptor)
+dirWDMapperRef = unsafePerformIO $ newIORef HM.empty
+
+usage :: IO ()
+usage = do
+  progName <- getProgName
+  putStrLn $ usageStr progName
+    where usageStr progName = "usage: " ++ progName ++ " src dst"
+
+sync :: FilePath -> IO ()
+sync relPath = do
+  baseSrcDir <- readIORef baseSrcDirRef
+  baseDstDir <- readIORef baseDstDirRef
+  let srcDir = joinPath [baseSrcDir, relPath]
+  let dstDir = joinPath [baseDstDir, relPath]
+  putStrLn dstDir
+  putStrLn $ "Syncing " ++ srcDir ++ " to " ++ dstDir
+  callProcess "rsync" ["-av", srcDir ++ "/", dstDir ++ "/"]
+
+disappearEventView :: Event -> Maybe Event
+disappearEventView evt
+  | (Deleted {}) <- evt = Just evt
+  | (MovedOut {}) <- evt = Just evt
+  | otherwise = Nothing
+
+appearEventView :: Event -> Maybe Event
+appearEventView evt
+  | (Created {}) <- evt = Just evt
+  | (MovedIn {}) <- evt = Just evt
+  | otherwise = Nothing
+
+adjustINotify :: INotify -> FilePath -> Event -> IO ()
+adjustINotify inotify srcDir (appearEventView -> Just evt) =
+  when (isDirectory evt) $ do
+    let path = filePath evt
+    let extendedPath = joinPath [srcDir, path]
+    watch inotify extendedPath
+adjustINotify _ _ _ = return ()
+
+
+handleEvent :: INotify -> FilePath -> Event -> IO ()
+
+handleEvent _ relPath (disappearEventView -> Just evt) = do
+  baseDstDir <- readIORef baseDstDirRef
+  let path = filePath evt
+  let extendedPath = joinPath [relPath, path]
+  let fullPath = joinPath [baseDstDir, extendedPath]
+  callProcess "rm" ["-rf", fullPath]
+  wd <- unmapWatch extendedPath
+  removeWatch wd
+
+handleEvent inotify relPath evt = do
+  let path = filePath evt
+  adjustINotify inotify relPath evt
+  if not $ isDirectory evt
+    then
+    sync relPath
+    else
+    let extendedPath = joinPath [relPath, path] in
+    sync extendedPath
+
+watch :: INotify -> FilePath -> IO ()
+watch inotify relPath = do
+  putStrLn ("Adding " ++ relPath ++ " to watch list")
+  baseSrcDir <- readIORef baseSrcDirRef
+  let absolutePath = joinPath [baseSrcDir, relPath]
+  wd <- addWatch inotify eventVarieties absolutePath (handleEvent inotify relPath)
+  dirWDMapper <- readIORef dirWDMapperRef
+  writeIORef dirWDMapperRef $ HM.insert relPath wd dirWDMapper
+
+mapWatch :: FilePath -> WatchDescriptor -> IO ()
+mapWatch relPath wd = do
+  dirWDMapper <- readIORef dirWDMapperRef
+  writeIORef dirWDMapperRef $ HM.insert relPath wd dirWDMapper
+
+unmapWatch :: FilePath -> IO WatchDescriptor
+unmapWatch relPath = do
+  dirWDMapper <- readIORef dirWDMapperRef
+  let wd = HM.lookup relPath dirWDMapper
+  writeIORef dirWDMapperRef $ HM.delete relPath dirWDMapper
+  case wd of
+    (Just wd') -> return wd'
+    _ -> error $ "Unexpected missing of watch descriptor for path " ++ relPath
 
 main :: IO ()
 main = do
@@ -91,57 +177,3 @@ main = do
   where
     extractDirs args
       | srcDir:dstDir:_ <- args = (srcDir, dstDir)
-
-usage :: IO ()
-usage = do
-  progName <- getProgName
-  putStrLn $ usageStr progName
-    where usageStr progName = "usage: " ++ progName ++ " src dst"
-
-sync :: FilePath -> IO ()
-sync relPath = do
-  baseSrcDir <- readIORef baseSrcDirRef
-  baseDstDir <- readIORef baseDstDirRef
-  let srcDir = joinPath [baseSrcDir, relPath]
-  let dstDir = joinPath [baseDstDir, relPath]
-  putStrLn dstDir
-  putStrLn $ "Syncing " ++ srcDir ++ " to " ++ dstDir
-  callProcess "rsync" ["-av", srcDir ++ "/", dstDir ++ "/"]
-
-adjustINotify :: INotify -> FilePath -> Event -> IO ()
-adjustINotify inotify srcDir (Created {isDirectory=True, filePath=path}) = do
-  let extendedPath = joinPath [srcDir, path]
-  watch inotify extendedPath
-adjustINotify _ _ _ = return ()
-
-deleteEventView :: Event -> Maybe Event
-deleteEventView evt
-  | DeletedSelf <- evt = Just evt
-  | (Deleted _ _) <- evt = Just evt
-  | otherwise = Nothing
-
-handleEvent :: INotify -> FilePath -> Event -> IO ()
-
-handleEvent _ relPath (deleteEventView -> Just evt) = do
-  let path = filePath evt
-  baseDstDir <- readIORef baseDstDirRef
-  let fullPath = joinPath [baseDstDir, relPath, path]
-  callProcess "rm" ["-rf", fullPath]
-
-handleEvent inotify relPath evt = do
-  let path = filePath evt
-  adjustINotify inotify relPath evt
-  if not $ isDirectory evt
-    then
-    sync relPath
-    else
-    let extendedPath = joinPath [relPath, path] in
-    sync extendedPath
-
-watch :: INotify -> FilePath -> IO ()
-watch inotify relPath = do
-  putStrLn ("Adding " ++ relPath ++ " to watch list")
-  baseSrcDir <- readIORef baseSrcDirRef
-  let absolutePath = joinPath [baseSrcDir, relPath]
-  wd <- addWatch inotify eventVarieties absolutePath (handleEvent inotify relPath)
-  return ()
